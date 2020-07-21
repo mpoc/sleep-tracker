9b9585d5fb10c1d07cdb69859b20c5bba895bd16
Marius <marius.pocev@gmail.com>
2020-07-21 23:11:14 +0300

    Refactor sleep.js

--- src/views/js/sleep.js
+++ src/views/js/sleep.js
@@ -20,54 +20,91 @@ const submit = position => {
     },
   };
 
-  const searchParams = new URLSearchParams(window.location.search);
-  if (!searchParams.has('apiKey')) {
-    document.getElementById('text').innerHTML = 'No API key provided';
-    return;
-  }
-  const apiKey = searchParams.get('apiKey');
+  const apiKey = getApiKey();
+  if (!apiKey) return;
 
-  const url = new URL('api/sleep', window.location.href + '/');
-  url.searchParams.append('apiKey', apiKey);
+  const url = constructApiUrl(apiKey);
 
   fetch(url, options)
     .then(res => res.json())
-    .then(data => {
-      if (data.success) {
-        const entries = Object.entries(data.data.updatedRow).map(([key, value]) => `${key}: ${value}`);
-        document.getElementById('text').innerHTML =
-          `Inserted row:<br>${entries.join(",<br>")}`;
-        const timeDiff = new Date() - new Date(data.data.updatedRow['Timezone local time']);
-        console.log(`${timeDiff / 1000} seconds ago.`)
-      } else {
-        console.error(data);
-        document.getElementById('text').innerHTML = data.message;
-      }
-    })
-    .catch(err => {
-      console.error(err);
-    });
+    .then(processApiResponse)
+    .catch(err => console.error(err));
 };
 
-const REQUIRED_ACCURACY = 20;
+const processApiResponse = data => {
+  if (data.success) {
+    const insertedRowText =
+      Object.entries(data.data.updatedRow)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join(",<br>");
+
+    document.getElementById('text').innerHTML = `Inserted row:<br>${insertedRowText}`;
+
+    const timeDiff = new Date() - new Date(data.data.updatedRow['Timezone local time']);
+    console.log(`${timeDiff / 1000} seconds ago.`)
+  } else {
+    console.error(data);
+    document.getElementById('text').innerHTML = data.message;
+  }
+}
+
+const getApiKey = () => {
+  const searchParams = new URLSearchParams(window.location.search);
+  if (!searchParams.has('apiKey')) {
+    document.getElementById('text').innerHTML = 'No API key provided';
+    return null;
+  }
+  const apiKey = searchParams.get('apiKey');
+  return apiKey;
+}
+
+const constructApiUrl = apiKey => {
+  const url = new URL('api/sleep', window.location.href + '/');
+  url.searchParams.append('apiKey', apiKey);
+  return url;
+}
+
+const REQUIRED_ACCURACY = 7000;
+// const REQUIRED_ACCURACY = 20;
 let watchID;
 
 const watchSuccess = position => {
-  const crd = position.coords;
+  printPosition(position);
+  checkAccuracy(position);
+}
 
-  console.log(
-    `Timestamp: ${new Date(position.timestamp)} (${position.timestamp})`
-  );
+const watchError = err => {
+  const errorText = `ERROR(${err.code}): ${err.message}`;
+  console.log(errorText);
+  document.getElementById('text').innerHTML = errorText;
+};
+
+const watchPosition = () => {
+  if (!geolocationAvailable()) return;
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  
+  watchID = navigator.geolocation.watchPosition(watchSuccess, watchError, options);
+}
+
+const printPosition = position => {
+  console.log(`Timestamp: ${new Date(position.timestamp)} (${position.timestamp})`);
   console.log('Your current position is:');
-  console.log(`Latitude: ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
+  console.log(`Latitude: ${position.coords.latitude}`);
+  console.log(`Longitude: ${position.coords.longitude}`);
+  console.log(`More or less ${position.coords.accuracy} meters.`);
+}
 
-  if (crd.accuracy > REQUIRED_ACCURACY) {
+const checkAccuracy = position => {
+  const accuracyAchieved = position.coords.accuracy > REQUIRED_ACCURACY;
+  if (accuracyAchieved) {
     document.getElementById(
       'text'
-    ).innerHTML = `Current accuracy: ${crd.accuracy} meters<br>Required accuracy: ${REQUIRED_ACCURACY} meters<br>Trying again...`;
-    return;
+    ).innerHTML = `Current accuracy: ${position.coords.accuracy} meters<br>Required accuracy: ${REQUIRED_ACCURACY} meters<br>Trying again...`;
   } else {
     document.getElementById('text').innerHTML = "Accuracy achieved, saving...";
     navigator.geolocation.clearWatch(watchID);
@@ -75,24 +112,13 @@ const watchSuccess = position => {
   }
 }
 
-const watchError = err => {
-  console.log(`ERROR(${err.code}): ${err.message}`);
-  document.getElementById('text').innerHTML = `ERROR(${err.code}): ${err.message}`;
-};
-
-
-const watchPosition = () => {
+const geolocationAvailable = () => {
   if (navigator.geolocation) {
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-    watchID = navigator.geolocation.watchPosition(watchSuccess, watchError, options);
+    return true;
   } else {
     console.log('Geolocation is not supported by this browser.');
-    document.getElementById('text').innerHTML =
-      'Geolocation is not supported by this browser.';
+    document.getElementById('text').innerHTML = 'Geolocation is not supported by this browser.';
+    return false;
   }
 }
 
