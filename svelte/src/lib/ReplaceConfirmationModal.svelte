a67727a0bd66c914971ce958f36c1cb7313c3a8c
Marius <marius.pocev@gmail.com>
2023-04-19 20:51:14 +0100

    Port pug to Svelte components

---
+++ svelte/src/lib/ReplaceConfirmationModal.svelte
@@ -0,0 +1,29 @@
+<div
+    class="modal fade"
+    id="confirmationModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="confirmationModalLabel"
+    aria-hidden="true"
+>
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirm replace</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close"
+                    ><span aria-hidden="true">&times;</span></button
+                >
+            </div>
+            <div class="modal-body">Are you sure you want to replace the last sleep entry?</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button
+                ><button
+                    class="btn btn-primary"
+                    id="replaceLastSleepButton"
+                    type="button"
+                    data-dismiss="modal">Replace</button
+                >
+            </div>
+        </div>
+    </div>
+</div>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import ReplaceConfirmationModal from '$lib/ReplaceConfirmationModal.svelte';
+</script>
+
+<table class="table table-responsive table-borderless">
+    <tbody>
+        <tr>
+            <td>
+                <button id="logSleepButton" class="btn btn-success btn-lg" type="button" disabled
+                    >Log sleep entry</button
+                >
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button
+                    id="replaceConfirmationButton"
+                    class="btn btn-primary btn-lg"
+                    type="button"
+                    data-toggle="modal"
+                    data-target="#confirmationModal"
+                    disabled>Replace last sleep entry include</button
+                >
+                <ReplaceConfirmationModal />
+            </td>
+        </tr>
+        <tr>
+            <td id="text">Getting last sleep log...</td>
+        </tr>
+    </tbody>
+</table>
