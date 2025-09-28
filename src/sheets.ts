6351cb178a29fb77f70910a3c239412063a80eb2
mpoc <marius.pocev@gmail.com>
2025-09-28 14:09:59 +0100

    Format sheets.ts

--- src/sheets.ts
+++ src/sheets.ts
@@ -1,127 +1,143 @@
-import fs from 'fs';
-import readline from 'readline';
+import fs from "node:fs";
+import readline from "node:readline";
+import { OAuth2Client } from "google-auth-library";
+import type { sheets_v4 } from "googleapis";
 // https://github.com/googleapis/google-api-nodejs-client/issues/2187
-import { sheets } from 'googleapis/build/src/apis/sheets';
-import { OAuth2Client } from 'google-auth-library';
-import { ApiError } from './error';
-import type { sheets_v4 } from 'googleapis';
+import { sheets } from "googleapis/build/src/apis/sheets";
+import { ApiError } from "./error";
 
-const CRED_PATH = 'secret/credentials.json';
-const TOKEN_PATH = 'secret/token.json';
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const CRED_PATH = "secret/credentials.json";
+const TOKEN_PATH = "secret/token.json";
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
 export const getSheetsObj = async () => {
   try {
-    const cred = JSON.parse(fs.readFileSync(CRED_PATH, 'utf8'));
+    const cred = JSON.parse(fs.readFileSync(CRED_PATH, "utf8"));
     const auth = await authorize(cred);
-    return sheets({version: 'v4', auth});
+    return sheets({ version: "v4", auth });
   } catch (error) {
     throw new ApiError("Failed to login to Google", error);
   }
-}
+};
 
 export const getArray = async (
   sheetsObj: sheets_v4.Sheets,
   spreadsheetId: string,
   range: string
-): Promise<any[][]> => await new Promise((resolve, reject) => {
+): Promise<any[][]> =>
+  (await new Promise((resolve, reject) => {
     sheetsObj.spreadsheets.values.get(
-      {spreadsheetId, range},
+      { spreadsheetId, range },
       (err: any, res: any) => (err ? reject(err) : resolve(res.data.values))
     );
-  }) as any[][];
+  })) as any[][];
 
 export const getObjectArray = async (
   sheetsObj: sheets_v4.Sheets,
   spreadsheetId: string,
   range: string
-): Promise<any[]> => toObjectArray(await getArray(sheetsObj, spreadsheetId, range));
+): Promise<any[]> =>
+  toObjectArray(await getArray(sheetsObj, spreadsheetId, range));
 
 export const getObjectArrayHeader = async (
   sheetsObj: sheets_v4.Sheets,
   spreadsheetId: string,
   range: string
-): Promise<any[]> => toObjectArray(
-  await getArray(sheetsObj, spreadsheetId, range),
-  (await getArray(sheetsObj, spreadsheetId, '1:1'))[0]
-);
+): Promise<any[]> =>
+  toObjectArray(
+    await getArray(sheetsObj, spreadsheetId, range),
+    (await getArray(sheetsObj, spreadsheetId, "1:1"))[0]
+  );
 
 export const append = async (
   sheetsObj: sheets_v4.Sheets,
   spreadsheetId: string,
   range: string,
   values: string[][]
-): Promise<any> => {
-  return await new Promise((resolve, reject) => {
-    sheetsObj.spreadsheets.values.append({
-      spreadsheetId,
-      range,
-      valueInputOption: 'USER_ENTERED',
-      requestBody: {
-        values
-      }
-    },
-    (err: any, res: any) => (err ? reject(err) : resolve(res.data.updates))
+): Promise<any> =>
+  await new Promise((resolve, reject) => {
+    sheetsObj.spreadsheets.values.append(
+      {
+        spreadsheetId,
+        range,
+        valueInputOption: "USER_ENTERED",
+        requestBody: {
+          values,
+        },
+      },
+      (err: any, res: any) => (err ? reject(err) : resolve(res.data.updates))
     );
   });
-}
 
 export const update = async (
   sheetsObj: sheets_v4.Sheets,
   spreadsheetId: string,
   range: string,
   values: string[][]
-): Promise<any> => {
-  return await new Promise((resolve, reject) => {
-    sheetsObj.spreadsheets.values.update({
-      spreadsheetId,
-      range,
-      valueInputOption: 'USER_ENTERED',
-      requestBody: {
-        values
-      }
-    },
-    (err: any, res: any) => (err ? reject(err) : resolve(res.data))
+): Promise<any> =>
+  await new Promise((resolve, reject) => {
+    sheetsObj.spreadsheets.values.update(
+      {
+        spreadsheetId,
+        range,
+        valueInputOption: "USER_ENTERED",
+        requestBody: {
+          values,
+        },
+      },
+      (err: any, res: any) => (err ? reject(err) : resolve(res.data))
     );
   });
-}
 
-export const deleteRows = async (
-  sheetsObj: sheets_v4.Sheets,
-  spreadsheetId: string,
-  sheetId: number,
-  startIndex: number,
-  endIndex: number
-): Promise<any> => {
-  return await new Promise((resolve, reject) => {
-    sheetsObj.spreadsheets.batchUpdate({
-      spreadsheetId,
-      requestBody: {
-        requests: [
-          {
-            deleteDimension: {
-              range: {
-                sheetId,
-                dimension: 'ROWS',
-                startIndex,
-                endIndex
-              }
-            }
-          }
-        ]
-      }
-    },
-    (err: any, res: any) => (err ? reject(err) : resolve(res.data))
+export const deleteRows = async ({
+  sheetsObj,
+  spreadsheetId,
+  sheetId,
+  startIndex,
+  endIndex,
+}: {
+  sheetsObj: sheets_v4.Sheets;
+  spreadsheetId: string;
+  sheetId: number;
+  startIndex: number;
+  endIndex: number;
+}): Promise<any> =>
+  await new Promise((resolve, reject) => {
+    sheetsObj.spreadsheets.batchUpdate(
+      {
+        spreadsheetId,
+        requestBody: {
+          requests: [
+            {
+              deleteDimension: {
+                range: {
+                  sheetId,
+                  dimension: "ROWS",
+                  startIndex,
+                  endIndex,
+                },
+              },
+            },
+          ],
+        },
+      },
+      (err: any, res: any) => (err ? reject(err) : resolve(res.data))
     );
   });
-}
 
 export const deleteRow = async (
   sheetsObj: sheets_v4.Sheets,
   spreadsheetId: string,
   sheetId: number,
   rowIndex: number
-): Promise<any> => deleteRows(sheetsObj, spreadsheetId, sheetId, rowIndex - 1, rowIndex);
+): Promise<any> =>
+  deleteRows({
+    sheetsObj,
+    spreadsheetId,
+    sheetId,
+    startIndex: rowIndex - 1,
+    endIndex: rowIndex,
+  });
 
 const toObjectArray = (array: any[][], header?: any[]): any[] => {
   if (!header) {
@@ -129,19 +145,19 @@ const toObjectArray = (array: any[][], header?: any[]): any[] => {
   }
   const output = [] as any[];
 
-  array.forEach(el => {
+  for (const el of array) {
     const entry = {} as any;
     header.forEach((h, i) => {
       entry[h] = el[i] ? el[i] : undefined;
     });
     output.push(entry);
-  });
+  }
 
   return output;
-}
+};
 
 const authorize = async (cred: any): Promise<OAuth2Client> => {
-  const {client_secret, client_id, redirect_uris} = cred.installed;
+  const { client_secret, client_id, redirect_uris } = cred.installed;
   const oAuth2Client = new OAuth2Client(
     client_id,
     client_secret,
@@ -149,21 +165,23 @@ const authorize = async (cred: any): Promise<OAuth2Client> => {
   );
 
   try {
-    const token = JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf8'));
+    const token = JSON.parse(fs.readFileSync(TOKEN_PATH, "utf8"));
     oAuth2Client.setCredentials(token);
     return oAuth2Client;
-  } catch (e) {
+  } catch {
     return await getNewToken(oAuth2Client);
   }
-}
+};
 
-const getNewToken = async (oAuth2Client: OAuth2Client): Promise<OAuth2Client> => {
+const getNewToken = async (
+  oAuth2Client: OAuth2Client
+): Promise<OAuth2Client> => {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
+    access_type: "offline",
     scope: SCOPES,
   });
 
-  console.log('Authorize this app by visiting this url: ', authUrl);
+  console.log("Authorize this app by visiting this url: ", authUrl);
 
   return (await new Promise((resolve, reject) => {
     const rl = readline.createInterface({
@@ -171,7 +189,7 @@ const getNewToken = async (oAuth2Client: OAuth2Client): Promise<OAuth2Client> =>
       output: process.stdout,
     });
 
-    rl.question('Enter the code from that page here: ', code => {
+    rl.question("Enter the code from that page here: ", (code) => {
       rl.close();
       oAuth2Client.getToken(code, (err, token) => {
         reject(err);
@@ -186,4 +204,4 @@ const getNewToken = async (oAuth2Client: OAuth2Client): Promise<OAuth2Client> =>
       });
     });
   })) as OAuth2Client;
-}
+};
