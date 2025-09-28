f8a7c7a5b49b7a0b118b80b580922cb6f21f6e12
mpoc <marius.pocev@gmail.com>
2025-09-28 14:12:34 +0100

    Format checkReminderLoop

--- src/checkReminderLoop.ts
+++ src/checkReminderLoop.ts
@@ -1,6 +1,11 @@
 import { getLastSleep } from "./controller";
 import { sendReminderNotification } from "./notifications";
-import { millisecondsSinceSleepEntry, millisecondsToHours, minutesToMilliseconds, sheetsSleepEntryIsStop } from "./utils";
+import {
+  millisecondsSinceSleepEntry,
+  millisecondsToHours,
+  minutesToMilliseconds,
+  sheetsSleepEntryIsStop,
+} from "./utils";
 
 const HOURS_BEFORE_START_REMINDER = 15.5;
 const HOURS_BEFORE_STOP_REMINDER = 8.5;
@@ -11,17 +16,23 @@ let reminderNotificationSent = false;
 const checkReminderNotification = async () => {
   const lastSleepData = await getLastSleep();
 
-  const lastSleepEntryIsStop = sheetsSleepEntryIsStop(lastSleepData.lastSleepEntry);
+  const lastSleepEntryIsStop = sheetsSleepEntryIsStop(
+    lastSleepData.lastSleepEntry
+  );
 
   const msDiff = millisecondsSinceSleepEntry(lastSleepData.lastSleepEntry);
   const hoursDiff = millisecondsToHours(msDiff);
 
-  const hoursBeforeReminder = lastSleepEntryIsStop ? HOURS_BEFORE_START_REMINDER : HOURS_BEFORE_STOP_REMINDER;
+  const hoursBeforeReminder = lastSleepEntryIsStop
+    ? HOURS_BEFORE_START_REMINDER
+    : HOURS_BEFORE_STOP_REMINDER;
 
   // console.log(`${new Date().toISOString()}: Checking ${lastSleepEntryIsStop ? 'start' : 'stop'} reminder notification ${hoursDiff} (${hoursBeforeReminder})`)
   if (hoursDiff > hoursBeforeReminder) {
     if (!reminderNotificationSent) {
-      console.log(`${new Date().toISOString()}: Sending ${lastSleepEntryIsStop ? 'start' : 'stop'} reminder notification`)
+      console.log(
+        `${new Date().toISOString()}: Sending ${lastSleepEntryIsStop ? "start" : "stop"} reminder notification`
+      );
       reminderNotificationSent = true;
       await sendReminderNotification(msDiff, lastSleepEntryIsStop);
     }
@@ -32,5 +43,8 @@ const checkReminderNotification = async () => {
 
 export const checkReminderLoop = () => {
   checkReminderNotification();
-  setTimeout(checkReminderLoop, minutesToMilliseconds(REMINDER_CHECK_INTERVAL_MINUTES));
+  setTimeout(
+    checkReminderLoop,
+    minutesToMilliseconds(REMINDER_CHECK_INTERVAL_MINUTES)
+  );
 };
