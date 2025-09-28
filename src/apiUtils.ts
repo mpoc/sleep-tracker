1e98efde7ebad2e18be5ecc79bc561bc982af7cc
mpoc <marius.pocev@gmail.com>
2025-09-28 14:12:53 +0100

    Format apiUtils

--- src/apiUtils.ts
+++ src/apiUtils.ts
@@ -1,24 +1,30 @@
-import { ApiError } from './error'
+import type { ApiError } from "./error";
 import type { ApiResponse, SuccessResponseData } from "./types";
 
 export const successResponse = (data: SuccessResponseData, message: string) => {
   const response: ApiResponse = {
     success: true,
     data,
-    message
-  }
+    message,
+  };
   return Response.json(response);
-}
+};
 
 export const errorResponse = (error: ApiError) => {
   if (error.errorLogData instanceof Error) {
-    console.error(error.message + (error.errorLogData ? (':\n\t' + error.errorLogData.toString()) : ""));
+    console.error(
+      error.message +
+        (error.errorLogData ? `:\n\t${error.errorLogData.toString()}` : "")
+    );
   } else {
-    console.error(error.message + (error.errorLogData ? (':\n\t' + JSON.stringify(error.errorLogData)) : ""));
+    console.error(
+      error.message +
+        (error.errorLogData ? `:\n\t${JSON.stringify(error.errorLogData)}` : "")
+    );
   }
   const response: ApiResponse = {
     success: false,
-    message: error.message
+    message: error.message,
   };
   return Response.json(response);
 };
